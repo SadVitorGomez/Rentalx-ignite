207aa6d05ebf4dce934c547b6db2c215fb360c41
Vitor <7vitorgomez@gmail.com>
2021-12-12 09:28:26 -0300

    Specifications table created

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,17 @@
-import { Request, Response, Router } from 'express';
-import CategoriesRepository from '../repositories/CategoriesRepository';
-import CreateCategoryService from '../service/CreateCategoryService';
+import { Router } from 'express';
+import CategoriesRepository from '../modules/cars/repositories/CategoriesRepository';
+import CreateCategoryService from '../modules/cars/service/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.get('/', (req: Request, res: Response) => {
+categoriesRoutes.get('/', (req, res) => {
   const allCategories = categoriesRepository.list();
 
   return res.json(allCategories);
 });
 
-categoriesRoutes.post('/', (req: Request, res: Response) => {
+categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
   const createCategoryService = new CreateCategoryService(categoriesRepository);
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import SpecificationsRepository from '../modules/cars/repositories/SpecificationsRepository';
+import CreateSpecificationService from '../modules/cars/service/CreateSpecificationService';
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.get('/', (req, res) => {
+  const allSpecifications = specificationsRepository.list();
+
+  return res.json(allSpecifications);
+});
+
+specificationsRoutes.post('/', (req, res) => {
+  const { name, description } = req.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository,
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return res.status(201).send();
+});
+
+export default specificationsRoutes;

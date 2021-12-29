37bdecf2744f5d7a488e8c63193bcbdc085148d6
Vitor <7vitorgomez@gmail.com>
2021-12-29 02:48:29 -0300

    feat: created a middleware which requires auth token to access routes

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -10,6 +10,7 @@ interface ICreateUsersDTO {
 interface IUsersRepository {
   create(data: ICreateUsersDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
 
 export { ICreateUsersDTO, IUsersRepository };
